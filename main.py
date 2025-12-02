5d28fc25453c6d265e7a6f904b230201914e080f
Maniking <ameibomk@gmail.com>
2025-12-02 22:25:00 +0100

    main.py update to return number of words

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def words_in_book(book_text):
+    words = book_text.split()
+    num_words = len(words)
+    return num_words
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = words_in_book(book_text)
+    print(f"Found {num_words} total words")
+
+main()
