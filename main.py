37a54359f4f67cc1f204bd013af17a8984bc2389
Maniking <ameibomk@gmail.com>
2025-12-05 20:25:00 +0100

    Updated books and changed main to test any book

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-def get_book_text(path):
-    with open(path) as f:
-        file_contents = f.read()
-    return file_contents
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit[1]
 
-def words_in_book(book_text):
-    words = book_text.split()
-    num_words = len(words)
-    return num_words
+book_path = sys.argv[1]
+
+from stats import get_num_words, get_num_chars, chars_dict_to_sorted_list
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = words_in_book(book_text)
+    text = get_book_text(book_path)
+
+    num_words = get_num_words(text)
+    num_chars = get_num_chars(text)
+
+    sorted_list = chars_dict_to_sorted_list(num_chars)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted_list:
+        ch = item["char"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
+
+    print("============= END ===============")
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 main()
---
+++ stats.py
@@ -0,0 +1,25 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_num_chars(chars):
+    chars = chars.lower()
+    characters = {}
+
+    for char in chars:
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+            
+    return characters
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
